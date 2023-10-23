15c903fda83a8321bfa416d6e6c2e9d7fd8cf248
crist <crist@MSI>
2023-10-23 21:19:21 +0200

    Agregar archivos de proyecto.

---
+++ SuperHeroesApp/Models/SuperHeroe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroesApp.Models
+{
+    //Clase SuperHeroe    
+    class SuperHeroe : Heroe
+    {
+        public int Id;        
+        private string _Nombre;
+        public override string Nombre {  get { return _Nombre; } set { _Nombre = value.Trim(); }}
+        public string IdentidadSecreta;
+        public string NombreEIdentidadSecreta {get{return $"{Nombre}({IdentidadSecreta})";}}        
+        public string Ciudad;
+        public List<SuperPoder> SuperPoderes;
+        public bool PuedeVolar;
+
+        //Método constructor, puedo crear otros métodos que me ayuden a definir el comportamiento de nuestros objetos
+        public SuperHeroe()
+        {
+            Id = 1;
+            SuperPoderes = new List<SuperPoder>();
+            PuedeVolar = false;
+        }
+
+        internal string UsarSuperPoderes()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var poder in SuperPoderes)
+            {
+                sb.AppendLine($"{NombreEIdentidadSecreta} esta usando el super poder de {poder.Nombre}!!");
+            }
+            return sb.ToString();
+        }
+
+        public override string SalvarElMundo()
+        {
+            return $"{NombreEIdentidadSecreta} ha salvado el mundo";
+        }
+
+        public override string SalvarLaTierra()
+        {
+            //return base.SalvarLaTierra();//con base estaría haciendo lo que hace el padre
+            return $"{NombreEIdentidadSecreta} ha salvado la tierra";
+        }
+    }
+}
---
+++ SuperHeroesApp/Program.cs
@@ -0,0 +1,70 @@
+﻿
+
+using System.Text;
+using SuperHeroesApp.Models;
+
+
+/*Pendiente por hacer los atributos privados y crear clases para la impresión de estos*/
+var poderVolar = new SuperPoder();
+poderVolar.Nombre = "Volar";
+poderVolar.Descripcion = "Capacidad para volar y planear en el aire";
+poderVolar.Nivel = NivelPoder.NivelDos;
+
+var superFuerza = new SuperPoder();
+superFuerza.Nombre = "Super fuerza";
+superFuerza.Nivel = NivelPoder.NivelTres;
+
+var regeneracion = new SuperPoder();
+regeneracion.Nombre = "Regeneración";
+regeneracion.Nivel = NivelPoder.NivelDos;
+
+SuperPoder telaraña = SuperPoder.CreaPoder("telaraña", "Permite lanzar un tejido para desplazarse a través de este", NivelPoder.NivelUno);
+SuperPoder volar = SuperPoder.CreaPoder("Volar", "Capacidad para volar y planear en el aire",NivelPoder.NivelDos);
+SuperPoder volar2 = SuperPoder.CreaPoder("Volareeeee", "Capacidad para volar y planear en el aire", NivelPoder.NivelDos);
+
+//telarania.CreaPoder("nombre 1","descripcion 1",NivelPoder.NivelDos);
+
+//Creo un objeto de lista llamado poderesSuperman
+List<SuperPoder> poderesSuperman = new List<SuperPoder>();
+poderesSuperman.Add(volar);
+poderesSuperman.Add(superFuerza);
+poderesSuperman.Add(telaraña);
+
+//Creo un objeto de lista llamado poderesSuperman
+List<SuperPoder> poderesWolverine = new List<SuperPoder>();
+poderesWolverine.Add(superFuerza);
+poderesWolverine.Add(regeneracion);
+
+//Creación de SuperHeroe
+var superman = new SuperHeroe();
+superman.Id = 1;
+superman.Nombre = "     Superman        ";
+superman.IdentidadSecreta = "Clark Kent";
+superman.Ciudad = "Metrópolis";
+superman.PuedeVolar = true;
+superman.SuperPoderes = poderesSuperman;
+string ResultSuperman = superman.UsarSuperPoderes();
+Console.WriteLine(ResultSuperman);
+string resultSalvarMundo = superman.SalvarElMundo();
+Console.WriteLine(resultSalvarMundo);
+string resultSalvarTierra = superman.SalvarLaTierra();
+Console.WriteLine(resultSalvarTierra);
+
+
+//Creación AntiHeroe para probar la Herencia como concepto
+var wolverine = new AntiHeroe();
+wolverine.Id = 2;
+wolverine.Nombre = "  Wolverine";
+wolverine.IdentidadSecreta = "Logan";
+wolverine.PuedeVolar = false;
+wolverine.SuperPoderes = poderesWolverine;
+string ResultWolverine = wolverine.UsarSuperPoderes();
+Console.WriteLine(ResultWolverine);
+
+string accionAntiHeroe = "Ataque a la policia";
+string delito = wolverine.CometerDelito(accionAntiHeroe);
+Console.WriteLine(delito);
+
+
+
+
